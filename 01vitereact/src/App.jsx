7a1531cb35e1b1cebb63d57cd26699f366ebd954
Omkar <Omkaar1>
2025-01-30 15:35:01 +0530

    compared vite and CRA

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Pizza from "./pizza";
+
+function App() {
+  return (
+    <>
+      <Pizza />
+      <h1>Pizza and coffee</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/pizza.jsx
@@ -0,0 +1,5 @@
+function pizza() {
+  return <h2>Breakfast is ready</h2>;
+}
+
+export default pizza;
